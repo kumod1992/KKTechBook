25196f48b053bbe501af95cbe2bd3b653edb0101
kumod1992 <kumodkumar01@gmail.com>
2021-04-27 16:28:50 +0530

    added new project

---
+++ LightningWebComponent/force-app/main/default/lwc/myFirstLWC/myFirstLWC.js
@@ -0,0 +1,7 @@
+import {LightningElement, track ,api} from 'lwc';
+
+export default class MyFirstLWC extends LightningElement {
+@api Name='kumod'
+@track title='CRM Consultant'
+phone='9035931168'
+}
